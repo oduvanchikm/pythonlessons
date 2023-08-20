8193ded9e36de677a26903ea194f0d1ab4281d75
Mira Guabreva <mkgubareva2005@gmail.com>
2023-08-20 20:05:27 +0300

    new

---
+++ yandex_academy/first/16.py
@@ -0,0 +1,7 @@
+A = int(input())
+B = int(input())
+C = int(input())
+
+t = (B - A) / C
+
+print(t)
---
+++ yandex_academy/first/17.py
@@ -0,0 +1,6 @@
+digit_10 = int(input())
+digit_2 = int(input(), 2)
+
+digit = digit_10 + digit_2
+
+print(digit)
